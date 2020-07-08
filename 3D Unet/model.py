cdce61ea87aa8412c2beb656730f46badcca5ce9
Linus Meienberg <linusm@ethz.ch>
2020-07-08 17:48:20 +0200

    implement input block

--- 3D Unet/model.py
+++ 3D Unet/model.py
@@ -10,23 +10,67 @@ Implementation details inspired by the model code a the NVIDIA Deep Learning Exa
 
 import tensorflow as tf 
 
-    """ When tf.keras.Model is subclassed:
+""" When tf.keras.Model is subclassed:
 
-    Use the constructor method __init__ to instantiate the layers as variables of the model instance
-    Use the functional API to configure them and provide inputs as arguments at construction time
-    return the model output
+Use the constructor method __init__ to instantiate the layers as variables of the model instance
+layer parameters are specified but the input shape is passed in the call function
 
-    define the method call() providing the input arguments for evaluation of the model
-    pass them stepwise through the model layers
-    return the output of the block / model
+
+define the method call() providing the input arguments for evaluation of the model
+pass them stepwise through the model layers
+return the output of the block / model
+
+"""
+
+def _crop_and_concat(inputs, residual_input):
+    """Perform a central crop of the skip connection so that it can be oncatenated to the inputs
+
+    Parameters
+    ----------
+    inputs : tf.Tensor
+        tensor with inputs (normally from the lower layer)
+    residual_input : tf.Tensor
+        tensor holding fine grained information for reconstruction. Was diverted from a corresponding downsampling block
 
     """
 
-def _crop_and_concat(inputs, residual_input):
-    #TODO a method that performs a central crop of the residual input and concatenates it to the inputs 
+    crop_ratio = inputs.shape[1] / residual_input.shape[1] # The ratio of the residual inputs that is retained
+    cropped = tf.image.central_crop(residual_input, crop_ratio)
+    concatenated = tf.concat([inputs, cropped], axis=-1) # Join the overlapping 3D regions by concatenating the channel (last) axis
+    return concatenated
 
 class InputBlock(tf.keras.Model):
     #TODO a class that implements a keras model (can be used as building block) and bundles the input operations of the Unet 
+    def __init__(self, initial_filters):
+        """Unet Input Block
+
+        Performs:
+        Convolution of input image with #initial_filters
+        Convolution doubling the #filters
+        Divert output for skip connection
+        Downsample by Max Pooling
+
+        Parameters
+        ----------
+        initial_filters : int
+            the number of initial convolution filters. Grows exponentially with model depth.
+        """
+        super().__init__()
+        # Instantiate Block 
+        with tf.name_scope('input_block'):
+            self.conv1 = tf.keras.layers.Conv3D(filters = initial_filters,
+                                                kernel_size=(3,3,3),
+                                                activation = tf.nn.relu)
+            self.conv2 = tf.keras.layers.Conv3D(filters= initial_filters*2,
+                                                kernel_size= (3,3,3),
+                                                activation=tf.nn.relu)
+            self.maxpool = tf.keras.layers.MaxPool3D(pool_size=(2,2,2), strides= 2)
+
+    def call(self, inputs):
+        x = self.conv1(inputs)
+        x = self.conv2(x)
+        out = self.maxpool(x)
+        return out, x # Provide full res intermediate x for skip connection
 
 class DownsampleBlock(tf.keras.Model):
     #TODO a class that bundels the steps of one downsampling BlockingIOError
