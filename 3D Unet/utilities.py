fff53413b4e7a87e7fcb58bbf7ac06d95fdacd22
Linus Meienberg <linusm@ethz.ch>
2020-07-09 16:32:07 +0200

    Implement Unet Architecture Blocks

---
+++ 3D Unet/utilities.py
@@ -0,0 +1,66 @@
+"""Collection of utilities to run 3D Unet
+"""
+
+
+def check_image_size(size, n_blocks):
+    """Checks if a valid unet architecture with n blocks can be constructed from an image input 
+
+    Parameters
+    ----------
+    size : int    
+        side length of input volume (cube)
+    n_blocks : int
+        the number of blocks in the downsampling and upsampling path
+
+    Returns
+    -------
+    boolean, int
+        validity, size of output image (0 if false)
+    """
+    x = size
+    outputs = []
+
+    # Input Block
+    x -= 4 # two convolution operations
+    outputs.append(x)
+    # downsampling
+    if not x%2==0: # check if 2x2 max pooling tiles nicely
+            #print('Input block: max pool input not divisible by 2')
+            return False, 0
+    x /= 2
+
+    # downsampling
+    for n in range(n_blocks):
+        x -= 4 # two conv layers 3x3
+        outputs.append(x) # store output dimension
+        if not x%2==0: # check if 2x2 max pooling tiles nicely
+            #print('Down {} max pool input {} not divisible by 2'.format(n+1,x))
+            return False, 0
+        x /= 2
+
+    
+    # bottleneck block
+    x -= 4
+    x *= 2
+
+    # upsampling
+    for n in range(n_blocks):
+        skip = outputs.pop()
+        if not (skip-x)%2==0:
+            print('Up {} crop from {} to {} not centered'.format(n,skip,x))
+            return False, 0
+        x -= 4 # two conv layers 3x3
+        x *= 2 # upsampling
+    
+    # output block
+    skip = outputs.pop()
+    if not (skip-x)%2==0:
+        print('Output block: crop from {} to {} not centered'.format(skip,x))
+        return False, 0
+    x-=4
+
+    #print('image size valid')
+    if x>0:
+        return True, x
+    else:
+        return False, 0
