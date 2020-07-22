02332337fead94df225d63b3b60be41f6d1f68c7
Linus Meienberg <linusm@ethz.ch>
2020-07-22 14:36:54 +0200

    work on 3D tiling

---
+++ tools/metrics.py
@@ -0,0 +1,45 @@
+"""A collection of evaluation metrics to assess the performance in 3D image segmentation tasks
+   Linus Meienberg
+   July 2020
+"""
+#%%
+import numpy as np
+
+def intersection_over_union(true_mask,predicted_mask,num_classes=2,smooth=1):
+    """Calculate the intersection over union metric for two sparse segmentation masks.
+
+    Parameters
+    ----------
+    true_mask : tensor
+        true segmentation mask in format (b,x,y,z,1) or (b,x,y,z)
+    predicted_mask : tensor
+        predicted segmentation mask in format (b,x,y,z,1) or (b,x,y,z)
+    num_classes : int, optional
+        the number of classes present in the segmentation mask, by default 2
+    smooth : float, optional
+        constant added to intersection and union. Smooths iou score for very sparse classes.
+
+    Returns
+    -------
+    list
+        the iou for each predicted class
+    """
+    if true_mask.shape[-1] == 1:
+        true_mask = true_mask[...,0]
+    if predicted_mask.shape[-1] == 1:
+        predicted_mask = predicted_mask[...,0]
+
+    assert true_mask.shape == predicted_mask.shape, 'segmentation masks do not match: shapes {} and {}'.format(true_mask.shape,predicted_mask.shape)
+
+    iou = []
+    # calculate iou for every class
+    for c in range(num_classes):
+        y_true = true_mask == c
+        y_pred = predicted_mask == c
+        intersection = np.sum(y_true & y_pred)
+        union = np.sum(y_true) + np.sum(y_pred) - intersection
+        iou.append((intersection+smooth)/(union+smooth))
+    
+    return iou
+
+# %%
--- tools/mosaic.py
+++ tools/mosaic.py
@@ -33,37 +33,33 @@ def showTiling(image, bounding_boxes):
     
     plt.show()
 
+#%%
 def rectangularTiling(image_shape, rectangle):
     """
-    Completely covers a target image with a rectangular tile
+    Completely covers a target image with a rectangular tile in two or three dimensions
 
     Parameters
     ----------
     image_shape : tuple
         the dimensions of the image that should be tiled
     rectangle : tuple
-        the dimensions of the rectangular tile (x,y)
+        the dimensions of the rectangular tile 
 
     Returns
     -------
     list
-        list of bounding boxes specified as 4-tuples (x,y,w,h)
+        list of bounding boxes specified as axis aligned bounding boxes (x,y,w,h) or (x,y,z,w,h,d)
     """
-    rows = np.ceil(image_shape[0]/rectangle[0]).astype(int)
-    cols = np.ceil(image_shape[1]/rectangle[1]).astype(int)
-    #print('rows {} cols {}'.format(rows,cols))
-    boxes = []
-    for r in range(rows):
-        # y coordinate of top left corner
-        x = r * rectangle[0]
-        for c in range(cols):
-            # x coordinate of top left corner
-            y = c * rectangle[1]
-            # Add bounding box to collection of tiles
-            bb = (x,y,rectangle[0],rectangle[1]) # (x,y,w,h) in image coordinates
-            boxes.append( bb ) #(x,y,w,h)
-    
-    return boxes
+    assert len(image_shape) == len(rectangle), 'incompatible rank'
+    assert len(image_shape) == 2 or len(image_shape)==3, 'specify two or three dimensional regions'
+    n_dim = len(image_shape)
+    shape = [np.ceil(image_shape[d]/rectangle[d]).astype(int) for d in range(n_dim)]
+    # get the coordinates of the regular grid which divides the image into rectangular regions
+    coords = [[n*rectangle[d] for n in range(shape[d])] for d in range(n_dim)]
+    mesh = np.meshgrid(*coords)
+    mesh_coords = np.stack([np.reshape(mesh[d], newshape=(-1,)) for d in range(n_dim)])
+    bb = [tuple(mesh_coords[:,i]) + rectangle for i in range(mesh_coords.shape[1])]
+    return bb
 
 #%%
 def image_to_plot(bounding_box):
@@ -385,6 +381,109 @@ image_reassembled = assembleTiles(inputTiles, regions['input_boxes'])
 image_reassembled_cropped = cropToBB(image_reassembled, (regions['offset'][0],regions['offset'][1], test_image.shape[0], test_image.shape[1]))
 showTiling(image_reassembled, regions['input_boxes'])
 showTiling(image_reassembled_cropped, regions['output_boxes'])
+"""
+# %%
+
+
+class UnetTiler3D():
+
+    def __init__(self, image, target, output_shape, input_shape):
+        """
+        Uses a pretrained 3D unet to segment an arbitraty input volume.
+
+        A rectangular tiling of the unet output is extended to a collection of input regions. 
+        The class handles reading and writing data from the h5 datasets that hold the image and segmentation mask.  
+
+        Parameters
+        ----------
+        image : h5py dataset
+            dataset containing the input tensor
+        target : h5py dataset
+            empty integer tensor where the segmentation mask is stored
+        output_shape : tuple
+            shape of the segmentation output of the unet
+        input_shape : tuple
+            shape of the image input of the unet
+        """
+        self.image = image
+        self.output_shape = output_shape
+        self.input_shape = input_shape
+        assert len(image.shape) == 3, 'Specify a single channel 3D image with format (x,y,z)'
+        assert len(output_shape) == 3, 'Specify the extent of the output shape as (x,y,z)'
+        assert len(input_shape) == 3, 'Specify the extent of the input shape as (x,y,z)'
+        
+        # Calculate the coordinate mesh of the tiling
+        # Each list goes up to the last multiple of tile_shape smaller than image_shape => endpoint excluded
+        self.x = list(range(0,image.shape[0],output_shape[0]))
+        self.y = list(range(0,image.shape[1],output_shape[1]))
+        self.z = list(range(0,image.shape[2],output_shape[2]))
+
+        # Expose the shape of the tiling
+        self.shape = (len(self.x),len(self.y),len(self.z))
+    
+    def __len__(self):
+        return np.prod(self.shape)
+        
+    def _getOutputTile(self, i):
+        """Returns an axis aligned boundary box defining the ith tile. 
+        The input image is tiled with the output of the unet to get a segmentation mask of the same size.
+
+        Parameters
+        ----------
+        i : int
+            index of the output tile
+
+        Returns
+        -------
+        tuple
+            aabb coordinate tuple (x0,y0,z0,x1,y1,z1) (diagonal oposite corners that define a rectangular volume)
+        """
+        # Sanity check
+        assert i >=0, 'index out of bounds'
+        assert i < len(self), 'index out of bounds'
+        # Convert index to the coordinates of the tile
+        x = i // (self.shape[1]*self.shape[2]) # number of elements that you skip by moving one position in dim 0
+        i = i % (self.shape[1]*self.shape[2])
+        y = i // self.shape[2]
+        z = i % self.shape[2]
+        # assemble the coordinates of the target chunk
+        x0 = self.x[x]
+        y0 = self.y[y]
+        z0 = self.z[z]
+        x1 = x0 + self.output_shape[0]
+        y1 = y0 + self.output_shape[1]
+        z1 = z0 + self.output_shape[2]
+        return (x0,y0,z0,x1,y1,z1)
+
+    def _getInputTile(self, i):
+        aabb = self.getOutputTile(i) # get the aabb of the corresponding input tile 
+        delta = np.subtract(self.input_shape,self.output_shape) // 2 # symmetric expansion in each direction
+        # we have to subtract delta from the inital coords and add it to the stop coords
+        delta = np.concatenate((-delta, delta))
+        aabb = np.add(aabb,delta) # element wise addition
+        return tuple(aabb)
+        
+    def _getSlice(self, aabb):
+        # clip the aabb if it protrudes from the image volume
+        start = [ np.max([0, d]) for d in aabb[:3] ] # origo is at (0,0,0)
+        stop = [ np.min([self.image.shape[i], aabb[i+3]]) for i in range(3) ]
+        # calculate the padding in each direction
+        pre_pad = [ np.max([0, -d]) for d in aabb[:3] ]
+        post_pad = [ np.max([0, aabb[i+3] - self.image.shape[i] ]) for i in range(3) ]
+        padding = tuple([ (pre_pad[i],post_pad[i]) for i in range(3) ] )
+        print('padding = {}'.format(padding))
+        # extract the cliped slices from the h5 archive
+        data = self.image[start[0]:stop[0],start[1]:stop[1],start[2]:stop[2]]
+        # pad the slice to the required size
+        data = np.pad(data, pad_width=padding, mode='constant')
+        return data 
+
+
+
 
 # %%
-"""
+test = UnetTiling3D(np.ones((10,20,30)), (5,5,5), (7,7,7))
+t = test.getInputTile(6)
+d = test._getSlice(t)
+
+# %%
