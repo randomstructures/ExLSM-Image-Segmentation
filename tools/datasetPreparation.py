3a45fafcec59b4a247fa03ca7b6879e02e9d1e6c
Linus Meienberg <linusm@ethz.ch>
2020-08-02 14:03:47 +0200

    change dataset format

--- tools/datasetPreparation.py
+++ tools/datasetPreparation.py
@@ -3,39 +3,37 @@
 
 import numpy as np
 import random
-import h5py, z5py
+import h5py
+from tqdm import tqdm
 
-import matplotlib.pyplot as plt
+# import tilingStrategy
 
-import mosaic
-
-
-def sampleMeanSignalStrength(tiler, n_samples):
+def getRandomIndices(tiler, n_samples):
     assert n_samples<len(tiler), 'Cannot sample more than {} samples from this tiling'.format(len(tiler))
-    sample_index = np.random.choice( np.arange(len(tiler)), size=n_samples, replace=False) # choose n_samples random chunks from the volume
-    sample_mean_signal = [ np.mean(tiler._cropAndPadAABB(tiler.image,tiler._getOutputTile(i))) for i in sample_index ]
+    sample_indices = np.random.choice( np.arange(len(tiler)), size=n_samples, replace=False) # choose n_samples random chunks from the volume
+    return sample_indices
 
-    return sample_index, sample_mean_signal
+def getMeanSignalStrengths(tiler, indices):
+    mean_signal_strengths = [ np.mean(tiler._cropAndPadAABB(tiler.image,tiler._getOutputTile(i))) for i in tqdm(indices) ]
+    return mean_signal_strengths
 
-def sampleMaskProportion(tiler, n_samples):
-    assert n_samples<len(tiler), 'Cannot sample more than {} samples from this tiling'.format(len(tiler))
-    sample_index = np.random.choice( np.arange(len(tiler)), size=n_samples, replace=False) # choose n_samples random chunks from the volume
+def sampleMaskProportion(tiler, indices):
     sample_volume = np.prod(tiler.output_shape) # number of pixels in the sample volume
     sample_mask_proportion = [ 
         np.count_nonzero(tiler._cropAndPadAABB(tiler.mask,tiler._getOutputTile(i))) / sample_volume
-         for i in sample_index ]
+         for i in tqdm(indices) ]
 
-    return sample_index, sample_mask_proportion
+    return sample_mask_proportion
 
 
-def thresholdedSampling(sample_index, sample_mean_signal, threshold, n_samples, object_ratio=0.5):
+def thresholdedSampling(indices, sample_mean_signal, threshold, n_samples, object_ratio=0.5):
     """Choose samples from a volume based on their average signal strength.
     Samples are split in two groups by a mean signal threshold. 
     Random samples are drawn from each of the two groups so that the one exceeding the threshold maskes of the specified object ratio.
 
     Parameters
     ----------
-    sample_index : list
+    indices : list
         list of sample indices
     sample_mean_signal : list
         list of mean signal strengths
@@ -53,5 +51,76 @@ def thresholdedSampling(sample_index, sample_mean_signal, threshold, n_samples, 
     # weight each sample by it's class probability
     proba = np.multiply(is_high, object_ratio/np.sum(is_high)) + np.multiply(np.subtract(1,is_high), (1-object_ratio)/(len(is_high)-np.sum(is_high)))
     #print(proba)
-    samples = np.random.choice(sample_index, size=n_samples, replace=False, p=proba)
+    samples = np.random.choice(indices, size=n_samples, replace=False, p=proba)
     return samples
+
+class Dataset():
+    """Utility class that maintains a dataset in h5 format
+
+    Creates or appends to a dataset in h5 file format.
+    Corresponding unet input tiles and mask output tiles are stored in a group:
+    dataset
+        -position a
+            -image a
+            -mask a
+        -position b
+            -image b 
+            -mask b
+    
+
+    """
+
+    def __init__(self, dataset_path, append=True):
+        super().__init__()
+        if append:
+            self.dataset_h5 = h5py.File(dataset_path)
+        else:
+            self.dataset_h5 = h5py.File(dataset_path, mode='x') # create new, fail if exists
+        # keep track of existing groups
+        if len(self.keys())>0:
+            print('Opened dataset with {} preexisting items. Overwriting items with the same name.'.format(len(self.keys())))
+
+    def keys(self):
+        return self.dataset_h5.keys()
+
+    def add_tiles(self, tiler, indices, preprocessingFunction= None, binarizeMask = False):
+        for index in tqdm(indices, desc='Tiles added'):
+            
+            # fetch the data
+            image, mask = tiler.getSlice(index), tiler.getMaskSlice(index)
+
+            # preprocess if necessary
+            if not preprocessingFunction is None:
+                image = preprocessingFunction(image)
+            if binarizeMask:
+                mask = np.clip(mask,0,1)
+
+            # check if the item allready exists
+            if str(index) in self.keys():
+                #print('Overwriting item {}'.format(index))
+                del self.dataset_h5[str(index)] # delete the group
+
+            # create the group and write image and masks datasets
+            self.dataset_h5.create_group(str(index))
+            self.dataset_h5[str(index)].create_dataset('image', data=image)
+            self.dataset_h5[str(index)].create_dataset('mask', data=mask)
+
+    def delete(self,key):
+        if type(key) is int:
+            key = str(key)
+        assert key in self.keys(), 'Key not contained in dataset'
+        del self.dataset_h5[key]
+
+    def get(self, key):
+        if type(key) is int:
+            key = str(key)
+        assert key in self.keys(), 'Key not contained in dataset'
+        image = self.dataset_h5[key]['image']
+        mask =  self.dataset_h5[key]['mask']
+        return (image, mask)
+
+    def close(self):
+        self.dataset_h5.close()
+
+
+
